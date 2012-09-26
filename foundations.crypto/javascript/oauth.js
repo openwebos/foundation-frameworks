6d6c8efa5888098640cb0c537aa3e5613fdc840b
Nitika Korla <nitika.korla@palm.com>
2012-09-26 12:24:13 -0700

    Added notice file and updated copyright info for oauth.js
    Open-webOS-DCO-1.0-Signed-off-by: Nitika Korla <nitika.korla@palm.com>
    
    Change-Id: Ia927ccf3cc68f1e3dde4035b58f83530a18c50c1

--- foundations.crypto/javascript/oauth.js
+++ foundations.crypto/javascript/oauth.js
@@ -1,5 +1,6 @@
 /*
  * Copyright 2008 Netflix, Inc.
+ * Copyright (c) <2012> Hewlett-Packard Development Company, L.P.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
